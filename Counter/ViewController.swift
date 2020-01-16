f145dff2f2ab773ef83c701c4ca9599ac0db6d1a
lekhaP <preetilekha4@gmail.com>
2020-01-16 14:48:00 -0500

    Added counter and related controls

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+
+
+    @IBAction func increment(_ sender: UIButton) {
+        count = count + 1
+        countLabel.text = "\(count)"
+    }
+
+
+    @IBAction func decrementButton(_ sender: UIButton) {
+        count = count - 1
+        countLabel.text = "\(count)"
+    }
+
+    @IBAction func reset(_ sender: UIButton) {
+        count = 0
+        countLabel.text = "\(count)"
+    }
+
+    var count = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
